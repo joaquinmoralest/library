763c0d993a982d864ec0e9085959015336f2f1f1
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-08-30 12:40:45 -0400

    Add books from modal and display it

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 const library = document.querySelector('#library')
 const addButton = document.querySelector('#add-button')
 const dialog = document.querySelector('#add-book')
+const inputTitle = document.querySelector('#input-title')
+const inputAuthor = document.querySelector('#input-author')
+const inputPages = document.querySelector('#input-pages')
+const inputToRead = document.querySelector('#radio-to-read')
+const inputRead = document.querySelector('#radio-read')
+const inputFiction = document.querySelector('#radio-fiction')
+const inputNonFiction = document.querySelector('#radio-nonfiction')
+const saveButton = document.querySelector('#saveButton')
+const form = document.querySelector('form')
+let titleValue = ''
+let authorValue = ''
+let pagesValue = 1
+let categoryValue = ''
+let isReadValue = false
 
 addButton.addEventListener('click', () => {
   dialog.showModal()
+})
+
+saveButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  addBookToLibrary()
+  dialog.close()
+})
+
+inputTitle.addEventListener('change', () => {
+  titleValue = inputTitle.value
+})
+
+inputAuthor.addEventListener('change', () => {
+  authorValue = inputAuthor.value
+})
+
+inputPages.addEventListener('change', () => {
+  pagesValue = inputPages.value
+})
+
+inputToRead.addEventListener('change', () => {
+  isReadValue = false
+})
+
+inputRead.addEventListener('change', () => {
+  isReadValue = true
+})
+
+inputFiction.addEventListener('change', () => {
+  categoryValue = 'fiction'
+})
+
+inputRead.addEventListener('change', () => {
+  categoryValue = 'nonfiction'
 })
 
 const myLibrary = [
@@ -30,15 +78,33 @@ const myLibrary = [
   },
 ]
 
-function Book() {
-
+function Book(title, author, category, pages, isRead) {
+  this.title = title
+  this.author = author
+  this.category = category
+  this.pages = pages
+  this.isRead = isRead
 }
 
 function addBookToLibrary() {
+  const newBook = new Book(titleValue, authorValue, categoryValue, pagesValue, isReadValue)
+  myLibrary.push(newBook)
+  console.log(newBook)
+  resetValues()
+  displayBooks()
+}
 
+function resetValues() {
+  titleValue = ''
+  authorValue = ''
+  categoryValue = ''
+  pagesValue = 1
+  isReadValue = false
+  form.reset()
 }
 
 function displayBooks() {
+  library.innerHTML = ''
   myLibrary.forEach(book => {
     const card = document.createElement('div')
     card.classList.add('card')
